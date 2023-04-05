6fdc26d7fee82797bdb9489607a9b38b00f5462a
atriatls <adria.tavares28@gmail.com>
2023-04-05 15:42:22 -0300

    Refactor: componentizando o formulário

--- src/components/MainForm/MainForm.vue
+++ src/components/MainForm/MainForm.vue
@@ -10,30 +10,11 @@
           type="text"
           class="input"
           placeholder="Qual tarefa você deseja iniciar?"
+          v-model="taskTitle"
         />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <section>
-            <strong>
-              {{ timePassed }}
-            </strong>
-          </section>
-          <button class="button" @click="play">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="stop">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <timer-crontrol @finished-task="finishTask"/>
       </div>
     </div>
   </div>
@@ -41,30 +22,22 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import TimerCrontrol from "../TimerControl/TimerCrontrol.vue";
 
 export default defineComponent({
   name: 'MainForm',
-  data () {
-    let timeSeconds = 0;
-    let timer = 0;
-    return {
-      timeSeconds,
-      timer
-    }
+  components: {
+    TimerCrontrol
   },
-  computed: {
-    timePassed () : string {
-      return new Date(this.timeSeconds * 1000).toISOString().substr(11, 8)
+  data () {
+    let taskTitle = ''
+    return {
+      taskTitle
     }
   },
   methods: {
-    play () {
-      this.timer = setInterval(() => {
-        this.timeSeconds++
-      }, 1000)
-    },
-    stop () {
-      clearInterval(this.timer)
+    finishTask (timeSeconds: number) : void {
+      this.taskTitle = ''
     }
   },
 })
---
+++ src/components/TimerControl/TimerCrontrol.vue
@@ -0,0 +1,60 @@
+<template>
+    <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <timer-form :timeSeconds="timeSeconds"/>
+          <button class="button" @click="play" :disabled="isTimerRunning">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button" @click="stop" :disabled="!isTimerRunning">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TimerForm from '../TimerForm/TimerForm.vue'
+
+export default defineComponent({
+  name: 'TimerControl',
+  emits: ['finishedTask'],
+  components: {
+    TimerForm
+  },
+  data () {
+    let timeSeconds = 0;
+    let timer = 0;
+    let isTimerRunning = false
+    return {
+      timeSeconds,
+      timer,
+      isTimerRunning
+    }
+  },
+  methods: {
+    play () {
+      this.isTimerRunning = true;
+      this.timer = setInterval(() => {
+        this.timeSeconds++
+      }, 1000)
+    },
+    stop () {
+      clearInterval(this.timer)
+      this.isTimerRunning = false;
+      this.$emit('finishedTask', this.timeSeconds)
+      this.timeSeconds = 0
+    }
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
