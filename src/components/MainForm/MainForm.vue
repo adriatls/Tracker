a7f12af467aa379e3578aa11cbb56ac5efc5f993
atriatls <adria.tavares28@gmail.com>
2023-04-05 14:33:32 -0300

    Feat: adcionando um temporizador a página

--- src/components/MainForm/MainForm.vue
+++ src/components/MainForm/MainForm.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="box">
     <div class="columns">
-        <div class="comlumn is-8"
-          role="form"
-          aria-label="Formulário para criação de uma nova tarefa"
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+        />
+      </div>
+      <div class="column">
+        <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
         >
-          <input
-            type="text"
-            class="input"
-            placeholder="Qual tarefa você deseja iniciar"
-          >
-          <div class="column">
-            <div class="is-flex is-align-items-center is-justify-content-space-between"> 
-              <section>
-                <strong>
-                  00:00:00
-                </strong>
-              </section>
-              <button class="button">
-                <span class="icon">
-                    <i class="fas fa-play"></i>
-                </span>
-                <span>play</span>
-              </button>
-              <button class="button">
-                  <span class="icon">
-                      <i class="fas fa-stop"></i>
-                  </span>
-                  <span>stop</span>
-              </button>
-            </div>
-          </div>
+          <section>
+            <strong>
+              {{ timePassed }}
+            </strong>
+          </section>
+          <button class="button" @click="play">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button" @click="stop">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +44,29 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: 'MainForm',
+  data () {
+    let timeSeconds = 0;
+    let timer = 0;
+    return {
+      timeSeconds,
+      timer
+    }
+  },
+  computed: {
+    timePassed () : string {
+      return new Date(this.timeSeconds * 1000).toISOString().substr(11, 8)
+    }
+  },
+  methods: {
+    play () {
+      this.timer = setInterval(() => {
+        this.timeSeconds++
+      }, 1000)
+    },
+    stop () {
+      clearInterval(this.timer)
+    }
+  },
 })
 </script>
 
